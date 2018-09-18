6ac60ad686d0a25bfa795766e8b5b3412101ba34
Gordiig <gordiig@gmail.com>
2018-09-18 20:37:35 +0300

    Made functions static

--- gfs.c
+++ gfs.c
@@ -3,13 +3,13 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-int __init initFS(void)
+static int __init initFS(void)
 {
     printk(KERN_INFO "Module is installed!\n");
     return 0;
 }
 
-void __exit exitFS(void)
+static void __exit exitFS(void)
 {
     printk(KERN_INFO "Module is removed!\n");
 }
