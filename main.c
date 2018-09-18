46672b81b1e5375f88d5451615323b9c2a8956df
Gordiig <gordiig@gmail.com>
2018-09-18 20:01:29 +0300

    Wrote a simple module

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+int __init initFS(void)
+{
+    printk(KERN_INFO "Module is installed!\n");
+    return 0;
+}
+
+void __exit exitFS(void)
+{
+    printk(KERN_INFO "Module is removed!\n");
+}
+
+module_init(initFS);
+module_exit(exitFS);
+
+MODULE_AUTHOR("Gordiig");
+MODULE_LICENSE("MIT");
